2236b175d7e7457db4aee7ddf507d9999052be47
Glenn Mercier <gmercier1986@hotmail.com>
2018-09-19 14:57:03 -0400

    intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { ErrorHandler, Injectable, Injector, NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { Pro } from '@ionic/pro';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+Pro.init('445800fb', {
+  appversion: '0.0.1'
+});
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    IonicErrorHandler,
+    {provide: ErrorHandler, useClass: MyErrorHandler}
+  ]
+})
+export class AppModule {}
